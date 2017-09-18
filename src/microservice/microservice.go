4d2a038247af6545c90107eebb7aafb453644b2d
Mike Mackrory <mike@echovue.com>
2017-09-17 23:36:42 -0700

    Add Content-Type Header

--- src/microservice/microservice.go
+++ src/microservice/microservice.go
@@ -43,6 +43,7 @@ func distanceHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         panic(err)
     }
+    w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(200)
     w.Write(resp)
 }
@@ -57,6 +58,7 @@ func aboutHandler (w http.ResponseWriter, r *http.Request) {
     if err != nil {
         panic(err)
     }
+    w.Header().Set("Content-Type", "application/json")
     w.WriteHeader(200)
     w.Write(resp)
 }
